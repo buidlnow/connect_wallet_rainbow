74f387e31f85324b5581a53b8f3a507ba7770e6c
razacodespython <srh.zaidi@outlook.com>
2023-03-11 20:42:05 +0100

    removed custom provider

--- pages/_app.js
+++ pages/_app.js
@@ -7,13 +7,11 @@ import {
 } from '@rainbow-me/rainbowkit';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
 import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, provider } = configureChains(
   [mainnet, polygon, optimism, arbitrum],
   [
-    alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
     publicProvider()
   ]
 );
